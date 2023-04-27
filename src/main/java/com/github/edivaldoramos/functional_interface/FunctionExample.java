6595726bd7c58ac9e3539896b9aa031f5e8787a8
Edivaldo <edivaldorsj@gmail.com>
2023-04-26 23:00:24 -0300

    adds function example

--- src/main/java/com/github/edivaldoramos/functional_interface/FunctionExample.java
+++ src/main/java/com/github/edivaldoramos/functional_interface/FunctionExample.java
@@ -1,4 +1,17 @@
 package com.github.edivaldoramos.functional_interface;
 
+import java.util.function.Function;
+
+/**
+ * Function is a functional interface that accepts one argument and produces a result.
+ * Is commonly used for data transformation
+ */
 public class FunctionExample {
+    /**
+     * Return the value length
+     */
+    public static Integer getStringLength(String value){
+        Function<String, Integer> lengthFunction = String::length;
+        return lengthFunction.apply(value);
+    }
 }
--- src/test/java/com/github/edivaldoramos/functional_interface/FunctionExampleTest.java
+++ src/test/java/com/github/edivaldoramos/functional_interface/FunctionExampleTest.java
@@ -1,7 +1,16 @@
 package com.github.edivaldoramos.functional_interface;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class FunctionExampleTest {
+
+    @Test
+    public void shouldSuccessfullyReturnStringLength() {
+        String id = "1209-23120380121-8493";
+        Integer length = FunctionExample.getStringLength(id);
+
+        Assertions.assertEquals(21, length);
+    }
 
 }
